b3de360acfbebf723715a72251b26d92769d71a9
Bloduc Spauter <3230695439@qq.com>
2024-05-17 21:37:53 +0800

    媒体管理，上传文件

--- xuecheng-main-media/src/main/java/com/bloducspauter/media/service/impl/MediaFileServiceImpl.java
+++ xuecheng-main-media/src/main/java/com/bloducspauter/media/service/impl/MediaFileServiceImpl.java
@@ -19,34 +19,33 @@ import io.minio.UploadObjectArgs;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 /**
- * @version 1.0
+ * @author Bloduc Spauter
+ * 
  */
 @Slf4j
 @Service
 public class MediaFileServiceImpl implements MediaService {
 
-    @Autowired
+    @Resource
     MediaFilesMapper mediaFilesMapper;
 
-    @Autowired
+    @Resource
     MinioClient minioClient;
 
-    @Autowired
+    @Resource
     MediaService currentProxy;
 
     //存储普通文件
@@ -72,24 +71,23 @@ public class MediaFileServiceImpl implements MediaService {
         // 获取数据总数
         long total = pageResult.getTotal();
         // 构建结果集
-        PageResult<MediaFiles> mediaListResult = new PageResult<>(list, total, pageParams.getPageSize());
-        return mediaListResult;
+        return new PageResult<>(list, total, pageParams.getPageSize());
 
     }
 
-    //根据扩展名获取mimeType
+    //根据扩展名获取文件类型那个
     private String getMimeType(String extension){
         if(extension == null){
             extension = "";
         }
         //根据扩展名取出mimeType
         ContentInfo extensionMatch = ContentInfoUtil.findExtensionMatch(extension);
-        String mimeType = MediaType.APPLICATION_OCTET_STREAM_VALUE;//通用mimeType，字节流
+        //通用mimeType，字节流
+        String mimeType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
         if(extensionMatch!=null){
             mimeType = extensionMatch.getMimeType();
         }
         return mimeType;
-
     }
 
     /**
@@ -98,23 +96,27 @@ public class MediaFileServiceImpl implements MediaService {
      * @param mimeType 媒体类型
      * @param bucket 桶
      * @param objectName 对象名
-     * @return
+     * @return boolean
      */
-    public boolean addMediaFilesToMinIO(String localFilePath,String mimeType,String bucket, String objectName){
+    public boolean addMediaFilesToMinIO(String localFilePath, String mimeType, String bucket, String objectName){
         try {
             UploadObjectArgs uploadObjectArgs = UploadObjectArgs.builder()
-                    .bucket(bucket)//桶
-                    .filename(localFilePath) //指定本地文件路径
-                    .object(objectName)//对象名 放在子目录下
-                    .contentType(mimeType)//设置媒体文件类型
+                    //桶
+                    .bucket(bucket)
+                    //指定本地文件路径
+                    .filename(localFilePath)
+                    //对象名 放在子目录下
+                    .object(objectName)
+                    //设置媒体文件类型
+                    .contentType(mimeType)
                     .build();
             //上传文件
             minioClient.uploadObject(uploadObjectArgs);
-            log.debug("上传文件到minio成功,bucket:{},objectName:{},错误信息:{}",bucket,objectName);
+            log.debug("上传文件到minio成功,bucket:{},objectName:{}",bucket,objectName);
             return true;
         } catch (Exception e) {
-           e.printStackTrace();
            log.error("上传文件出错,bucket:{},objectName:{},错误信息:{}",bucket,objectName,e.getMessage());
+            e.printStackTrace();
         }
         return false;
     }
@@ -122,14 +124,12 @@ public class MediaFileServiceImpl implements MediaService {
     //获取文件默认存储目录路径 年/月/日
     private String getDefaultFolderPath() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String folder = sdf.format(new Date()).replace("-", "/")+"/";
-        return folder;
+        return sdf.format(new Date()).replace("-", "/")+"/";
     }
     //获取文件的md5
     private String getFileMd5(File file) {
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
-            String fileMd5 = DigestUtils.md5Hex(fileInputStream);
-            return fileMd5;
+            return DigestUtils.md5Hex(fileInputStream);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -171,17 +171,14 @@ public class MediaFileServiceImpl implements MediaService {
 
 
     /**
-     * @description 将文件信息添加到文件表
      * @param companyId  机构id
      * @param fileMd5  文件md5值
      * @param uploadFileParamsDto  上传文件的信息
      * @param bucket  桶
      * @param objectName 对象名称
-     * @return com.xuecheng.media.model.po.MediaFiles
-     * @author Mr.M
-     * @date 2022/10/12 21:22
      */
     @Transactional
+    @Override
     public MediaFiles addMediaFilesToDb(Long companyId,String fileMd5,UploadFileParamsDto uploadFileParamsDto,String bucket,String objectName){
         //将文件信息保存到数据库
         MediaFiles mediaFiles = mediaFilesMapper.selectById(fileMd5);
