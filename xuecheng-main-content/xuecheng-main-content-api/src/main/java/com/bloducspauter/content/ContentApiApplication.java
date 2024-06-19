dac8646eb877b8b479dbb636a333674c0472c406
Bloduc Spauter <3230695439@qq.com>
2024-06-19 18:34:39 +0800

    跨域问题

--- xuecheng-main-content/xuecheng-main-content-api/src/main/java/com/bloducspauter/content/ContentApiApplication.java
+++ xuecheng-main-content/xuecheng-main-content-api/src/main/java/com/bloducspauter/content/ContentApiApplication.java
@@ -1,5 +1,8 @@
 package com.bloducspauter.content;
+import com.bloducspauter.content.config.RequestFilter;
+import com.bloducspauter.system.mapper.DictionaryMapper;
 import com.bloducspauter.system.service.CourseCategoryService;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,9 +11,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author Bloduc Spauter
  *
  */
-@SpringBootApplication(scanBasePackages = {"com.bloducspauter.content"}
-        ,scanBasePackageClasses = {CourseCategoryService.class})
+@SpringBootApplication(scanBasePackages = {"com.bloducspauter.content","com.bloducspauter.base"}
+        ,scanBasePackageClasses = {CourseCategoryService.class, RequestFilter.class})
 @EnableDiscoveryClient
+@MapperScan(basePackages = {"com.bloducspauter.content.mapper"},basePackageClasses = {DictionaryMapper.class})
 public class ContentApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContentApiApplication.class, args);
